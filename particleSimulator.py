73f48a17d6738b3998e858c17413e2ec27a1f528
Satwik <rsatwik@users.noreply.github.com>
2023-01-16 02:27:36 +0530

    Update particleSimulator.py

--- particleSimulator.py
+++ particleSimulator.py
@@ -1,6 +1,9 @@
-Class Particle:
+class Particle:
   def __init__(self, x, y, ang_vel):
     self.x=x
     self.y=y
     self.ang_vel=ang_vel
     
+class ParticleSimulator:
+  def __init__(self,particles):
+    self.particles=particles
