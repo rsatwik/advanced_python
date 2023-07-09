4390f76884af90d9b3b56ecf64de44b99d2c64d9
rsatwik <test@test.com>
2023-07-09 23:51:45 +0530

    stablized using file access

--- trying_locks.py
+++ trying_locks.py
@@ -1,22 +1,25 @@
-# watch -n 0.5 'python3 trying_locks.py'
-
 import time
 import multiprocessing
 
-def deposit(balance):
+def deposit(balance, lock):
     for i in range(100):
         time.sleep(0.01)
+        lock.acquire()
         balance.value = balance.value + 1
+        lock.release()
 
-def withdraw(balance):
+def withdraw(balance, lock):
     for i in range(100):
         time.sleep(0.01)
+        lock.acquire()
         balance.value = balance.value - 1
+        lock.release()
 
 if __name__ == '__main__':
     balance = multiprocessing.Value('i', 200)
-    d = multiprocessing.Process(target=deposit, args=(balance,))
-    w = multiprocessing.Process(target=withdraw, args=(balance,))
+    lock = multiprocessing.Lock()
+    d = multiprocessing.Process(target=deposit, args=(balance,lock))
+    w = multiprocessing.Process(target=withdraw, args=(balance,lock))
     d.start()
     w.start()
     d.join()
